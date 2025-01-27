1b06ccfcb44d8d986d8b7b5666e2c6cb53cc4149
ashish12114257 <126353996+ashish12114257@users.noreply.github.com>
2025-01-27 12:32:52 +0000

    02datatpe

---
+++ 01_Basics/02dataTypes.js
@@ -0,0 +1,18 @@
+// "use strict";  //treat all js code as newer version
+//alert(3+3)    // we are using nodejs, not browser
+
+let name = "Ashsih"
+let age = 22
+let loggedIn  = true
+
+// data types 
+// Numberbigint
+// boolean 
+// null
+// undefined
+//symbol ==> uniqueness
+//object
+ 
+console.log(typeof "ashish")
+console.log(typeof null)
+console.log(typeof undefined)
