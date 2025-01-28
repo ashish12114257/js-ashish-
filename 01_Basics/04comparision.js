d87ad51852cdfef9ade71dd117fa9a48a0eb5a9d
ashish12114257 <126353996+ashish12114257@users.noreply.github.com>
2025-01-28 13:02:51 +0000

    04comparision.js

---
+++ 01_Basics/04comparision.js
@@ -0,0 +1,19 @@
+// console.log(2>1);
+// console.log(2>=1);
+// console.log(2<1);
+// console.log(2==1);
+// console.log(2!=1);
+
+// console.log("2">1);
+// console.log("02">1);
+
+// console.log(null>0);
+// console.log(null==0);
+// console.log(null>=0);
+
+// console.log(undefined==0);
+// console.log(undefined>0);
+// console.log(undefined<0);
+
+//strict check i.e === that means it also check datatypes too
+console.log("2"===2);
