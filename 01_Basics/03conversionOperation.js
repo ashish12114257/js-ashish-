35cc109b8765cdf401e2e815ed69f3c8810925aa
ashish12114257 <126353996+ashish12114257@users.noreply.github.com>
2025-01-28 06:22:12 +0000

    03conversionOperation

---
+++ 01_Basics/03conversionOperation.js
@@ -0,0 +1,55 @@
+let score = "Ashish"
+// console.log(typeof score);
+// console.log(typeof(score));
+
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber)
+
+//"33" =>33
+//"33abc" =>NaN
+//true => 1; false =>0;
+
+let IsLoggedIn  = "ashish"
+let booleanIsLoggedIn = Boolean(IsLoggedIn);
+// console.log(booleanIsLoggedIn);
+
+//1 =>true; 0 => false;
+//"" = false
+//"ashish" => true
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+// console.log(stringNumber);
+// console.log(typeof stringNumber)
+
+
+//*************************************Operations****************
+
+let value = 3
+let nagValue = -value
+// console.log(nagValue)
+
+// console.log(2+2)
+// console.log(2*2)
+// console.log(3-1)
+// console.log(3**2)
+// console.log(9/5)
+// console.log(8%2)
+
+let str1 = "hello"
+let str2 = " ashish"
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1+"2");
+// console.log("1"+2+2);
+// console.log(1+2+"2"); 
+
+// console.log(+true) but don't do this 
+// console.log(+"")  but don't do this
+
+let gameCounter = 100
+gameCounter++;
+console.log(gameCounter);
