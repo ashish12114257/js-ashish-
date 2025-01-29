cf1f20086a00b398eccdb796fe885ecd161fbb8c
ashish12114257 <126353996+ashish12114257@users.noreply.github.com>
2025-01-29 05:24:32 +0000

    nums and maths

---
+++ 01_Basics/06num_maths.js
@@ -0,0 +1,9 @@
+const score = 400
+console.log(score);
+const balance = new Number(200);   //now we can easily convert it
+console.log(balance);
+
+console.log(balance.toString());
+console.log(typeof balance);
+
+
