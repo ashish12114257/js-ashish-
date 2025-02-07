b460254b60df85c166d0ac8d780fb636e3409e22
ashish12114257 <126353996+ashish12114257@users.noreply.github.com>
2025-02-07 12:55:26 +0000

    arrow

---
+++ 03_Basics/03arrow.js
@@ -0,0 +1,30 @@
+const user = {
+    username: "ashish kumar",
+    price: 542,
+    welcomeMessage: function(){
+        // console.log(` ${this.username}, welcome to the website`);
+    }
+}
+
+// user.welcomeMessage()
+// user.username = "sam"
+// console.log(this);
+// user.welcomeMessage()
+
+// function chai(){
+//     username: "hitesh"
+//     console.log(this);
+    
+// }
+
+// chai();
+
+// const chai = function(){
+//     let username = "ashish"
+//     console.log(this.usrname);
+// }
+// chai()
+
+// arrow function 
+
+const addTwo = 
