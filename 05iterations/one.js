b7b25f3d544b282d9ee1514303c950918a191741
ashish12114257 <126353996+ashish12114257@users.noreply.github.com>
2025-02-12 03:10:33 +0000

    for break and continue

---
+++ 05iterations/one.js
@@ -0,0 +1,37 @@
+// for loop 
+for(let i = 0; i<=10; i++){
+    const element = i;
+    if(element == 5){
+        // console.log("5 is best number");
+    }
+    // console.log(element);
+}
+// console.log(element);
+
+
+
+let myArray = ["flash", "batman", "superman"]
+// console.log(myArray.length);
+for(let i = 0; i<myArray.length; i++){
+    const element = myArray[i];
+    // console.log(element);
+    
+}
+
+// break and continue 
+for(let i = 0; i<=10; i++){
+    if(i == 5){
+        // console.log(`dected 5`);
+        break
+    }
+    // console.log(`value of i is ${i}`); 
+}
+
+
+for(let i = 0; i<=10; i++){
+    if(i == 5){
+        // console.log(`dected 5`);
+        continue
+    }
+    // console.log(`value of i is ${i}`); 
+}
